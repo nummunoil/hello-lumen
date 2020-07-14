d6f3e47ea131a5bfc40338e797b14edcdda04bc7
Oil Sudarat <sudarat@enres.co>
2020-07-14 19:00:33 +0700

    ch#2 The Response 1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,3 +26,9 @@ $app->get('/hello/{name}', ['middleware' => 'hello', function ($name) {
 $app->get('/request', function (Illuminate\Http\Request $request) {
     return "Hello " . $request->get('name', 'stranger');
 });
+
+
+$app->get('/response', function (Illuminate\Http\Request $request) {
+    return (new Illuminate\Http\Response('Hello stranger', 200))
+->header('Content-Type', 'text/plain');
+});
